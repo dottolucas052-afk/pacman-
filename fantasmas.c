ddaf3a39989af50fbec6a203be441b90ecee1ddd
dottolucas052-afk <dottolucas052@gmail.com>
2025-10-26 20:54:06 -0300

    Implement ghost movement and teleportation
    Added ghost functionality and teleportation mechanics.

--- fantasmas.c
+++ fantasmas.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "raylib.h"
+#include <stdlib.h>
+#include <time.h>
+
 
 typedef struct{
     int linha;
@@ -29,6 +32,27 @@ int main(){
             }
         }
     }
+    int qnt_f = 0;
+    for(int i = 0; i<20; i++){
+        for(int j = 0; j<40; j++){
+            if(mapa[i][j] == 'F'){
+                qnt_f++;
+                //mapa[i][j] = ' ';
+            }
+        }
+    }
+    posicao *fantasmas = malloc(sizeof(posicao) * qnt_f);
+    int indice_fantasma = 0; 
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 40; j++){
+            if(mapa[i][j] == 'F'){
+                fantasmas[indice_fantasma].linha = i;
+                fantasmas[indice_fantasma].coluna = j;
+                mapa[i][j] = ' ';
+                indice_fantasma++; 
+            }
+        }   
+    }
 
     posicao portais[10];
     int qnt_portais = 0;
@@ -43,6 +67,7 @@ int main(){
     }
 
     InitWindow(800, 400, "pacman");
+    srand(time(NULL));
     SetTargetFPS(10);
     int dirx, diry;
     while(!WindowShouldClose()){
@@ -52,24 +77,17 @@ int main(){
         //movimento
         if(IsKeyDown(KEY_UP)){
             x--;
-            dirx = 0;
-            diry = 1;
         }
         if(IsKeyDown(KEY_DOWN)){
             x++;
-            dirx = 0;
-            diry = 1;
         }
         if(IsKeyDown(KEY_RIGHT)){
             y++;
-            dirx = -1;
-            diry = 0;
         }
         if(IsKeyDown(KEY_LEFT)){
             y--;
-            dirx  = -1;
-            diry = 0;
-        }  
+        }
+        
         //controle de colisão
         if(mapa[x][y] == '#'){
             x = aux1;
@@ -80,6 +98,63 @@ int main(){
         }
         if(mapa[x][y] == 'o'){
             mapa[x][y] = ' ';
+        }
+        for(int i = 0; i< qnt_f; i++){
+            int aux1 = fantasmas[i].linha, aux2 = fantasmas[i].coluna;
+            int dx = 0, dy = 0;
+            int aleatorio = rand()%2;
+            if(aleatorio == 0){
+                if(rand() % 100 > 70){
+                if(x > fantasmas[i].linha){
+                    fantasmas[i].linha++;
+                }else if(x < fantasmas[i].linha){
+                    fantasmas[i].linha--;
+                }
+                if(y > fantasmas[i].coluna){
+                    fantasmas[i].coluna++;
+                }else if(y < fantasmas[i].coluna){
+                    fantasmas[i].coluna--;
+                }
+            }else{
+                int dir = rand() % 4;
+                if(dir == 0){
+                   fantasmas[i].linha++; 
+                }
+                if(dir == 1){
+                    fantasmas[i].linha--;
+                }
+                if(dir == 2){
+                    fantasmas[i].coluna++;
+                }
+                if(dir == 3){
+                    fantasmas[i].coluna--;
+                }
+
+            } 
+        }   
+            if(mapa[fantasmas[i].linha][fantasmas[i].coluna] == '#'){
+                fantasmas[i].coluna = aux2;
+                fantasmas[i].linha = aux1;
+            }           
+        
+            if(mapa[fantasmas[i].linha][fantasmas[i].coluna] == 'T'){
+                for(int i = 0; i< qnt_portais; i++){
+                    if(!(fantasmas[i].linha == portais[i].linha && fantasmas[i].coluna == portais[i].coluna)){
+                        if(fantasmas[i].linha == portais[i].linha ||fantasmas[i].coluna == portais[i].coluna){
+                            fantasmas[i].linha = portais[i].linha ;
+                            fantasmas[i].coluna = portais[i].coluna ;
+                            if(fantasmas[i].coluna == 0){
+                                fantasmas[i].coluna++;
+                            }else{
+                                fantasmas[i].coluna--;
+                            }
+                            break;
+                        }
+            
+                    }
+                }                
+            }
+            
         }
         //Teleporte
         if(mapa[x][y] == 'T'){
@@ -114,15 +189,19 @@ int main(){
                     DrawCircle(j*20 + 10, i*20 + 10, 10, GREEN);
                 }
         
-                if(mapa[i][j] == 'F'){
-                    DrawRectangle(j*20, i*20, 20, 20, PURPLE);
-                }
+                // if(mapa[i][j] == 'F'){
+                //     DrawRectangle(j*20, i*20, 20, 20, PURPLE);
+                // }
                 if(mapa[i][j] == 'T'){
                     DrawCircle(j*20 + 10, i*20 + 10, 7, ORANGE);
                 }
+                
             }
         }
         DrawRectangle(y*20, x*20, 20, 20, RED);
+        for(int i = 0; i<qnt_f; i++){
+            DrawRectangle(fantasmas[i].coluna*20, fantasmas[i].linha*20, 20, 20, PURPLE);
+        }
         EndDrawing();
     }
     CloseWindow();
@@ -130,3 +209,4 @@ int main(){
     return 0;
 
 }
+
