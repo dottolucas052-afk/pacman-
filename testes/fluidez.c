300b28228e04291d09655fac5b8a264713586172
dottolucas052-afk <dottolucas052@gmail.com>
2025-11-04 20:28:23 -0300

    Refactor and clean up fluidez.c code

--- testes/fluidez.c
+++ testes/fluidez.c
@@ -172,25 +172,24 @@ tipo_posicao checar_teleporte(tipo_objeto *personagem, tipo_posicao portais[], i
 }
 
 void verificar_colisao_pacman_fantasma(tipo_objeto *pacman, tipo_objeto array_fantasmas[], int qnt_f, bool power_up_ativo, tipo_posicao pos_inicial_pacman) {
-    for (int i = 0; i < qnt_f; i++) {
-        // Verifica se Pacman e o Fantasma[i] estão na mesma posição
-        if (pacman->posicao.linha == array_fantasmas[i].posicao.linha &&
-            pacman->posicao.coluna == array_fantasmas[i].posicao.coluna) {
-
-            if (power_up_ativo) {
-                // depois ver se eles teram uma base e se retornam na hora
-                array_fantasmas[i].posicao.linha =11;
-                array_fantasmas[i].posicao.coluna = 19;
+    for(int i = 0; i< qnt_f; i++){
+        bool colisao = (pacman->posicao.linha == array_fantasmas[i].posicao.linha && pacman->posicao.coluna == array_fantasmas[i].posicao.coluna);
+        bool troca = (pacman->posicao_anterior.linha == array_fantasmas[i].posicao.linha && pacman->posicao_anterior.coluna == array_fantasmas[i].posicao.coluna 
+        && array_fantasmas[i].posicao_anterior.linha == pacman->posicao.linha && array_fantasmas[i].posicao_anterior.coluna == pacman->posicao.coluna);
+        if(troca||colisao){
+             if (power_up_ativo) {
+                array_fantasmas[i].posicao.linha = 0;
+                array_fantasmas[i].posicao.coluna = 0;
                 
                 //Lógica de pontuação 
                 
                 
             } else {
                 pacman->posicao = pos_inicial_pacman;
-                // se nao tiver com poder ele que é comido 
-                // contador de vidas
+                
             }
         }
+        
     }
 }
 
@@ -415,3 +414,4 @@ int main() {
     return 0;
 
 }
+
