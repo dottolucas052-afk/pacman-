b7dcba787e24acd8f780958daf7a895ff028027d
Leo-terres <105955899+Leo-terres@users.noreply.github.com>
2025-11-05 13:48:02 -0300

    Add files via upload
    mapa e entidades centralizados

---
+++ testes/ghost_fluido.c
@@ -0,0 +1,227 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include "raylib.h"
+#include "funcoes_fluido.h"
+int main() {
+
+    // ~~~~ 1) Leitura do arquivo ~~~~ //
+    FILE *arq = fopen("mapa_pacman.txt", "r");
+    if (arq == NULL) {
+        printf("ERRO NA ABERTURA DO ARQUIVO\n");
+        return 1;
+    }
+
+    char mapa[20][41];
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 40; j++) {
+            int c = fgetc(arq);
+            while (c == '\r' || c == '\n') c = fgetc(arq); // ignora quebra de linha
+            if (c == EOF) c = ' ';
+            mapa[i][j] = (char)c;
+        }
+        mapa[i][40] = '\0';
+    }
+    fclose(arq);
+
+    // ~~~~ 2) Inicializações ~~~~ //
+    tipo_objeto *array_fantasmas = NULL;
+    int qnt_f = 0;
+
+    tipo_objeto pacman;
+
+    tipo_posicao pos_inicial_pacman;
+
+    // Conta portais (T)
+    int contadorT = 0;
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 40; j++)
+            if (mapa[i][j] == 'T') contadorT++;
+
+    tipo_posicao *portais = NULL;
+    int qnt_portais = 0;
+    if (contadorT > 0) portais = malloc(contadorT * sizeof(tipo_posicao));
+
+    // ~~~~ 3) Varre mapa e configura entidades ~~~~ //
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 40; j++) {
+            if (mapa[i][j] == 'F') {
+                qnt_f++;
+                array_fantasmas = realloc(array_fantasmas, qnt_f * sizeof(tipo_objeto));
+                array_fantasmas[qnt_f-1].tipo = FANTASMA;
+                array_fantasmas[qnt_f-1].posicao.linha = i;
+                array_fantasmas[qnt_f-1].posicao.coluna = j;
+                array_fantasmas[qnt_f-1].direcao_atual = CIMA;
+                array_fantasmas[qnt_f-1].proxima_direcao = CIMA;
+                array_fantasmas[qnt_f-1].andar = true;
+                array_fantasmas[qnt_f-1].teleportado = false;
+                mapa[i][j] = ' ';
+            }
+            if (mapa[i][j] == 'T') {
+                portais[qnt_portais].linha = i;
+                portais[qnt_portais].coluna = j;
+                qnt_portais++;
+            }
+            if (mapa[i][j] == 'P') {
+                pacman.posicao.linha = i;
+                pacman.posicao.coluna = j;
+
+                pos_inicial_pacman.linha = i;
+                pos_inicial_pacman.coluna = j;
+                pacman.posicao_anterior = pacman.posicao;
+                pacman.andar = false;
+                pacman.teleportado = false;                           
+            }
+        }
+    }
+
+    // ~~~~ 4) Inicializa Raylib ~~~~ //
+    const int CELULA = 20;
+    const int LARGURA = 40 * CELULA;
+    const int ALTURA = 20 * CELULA;
+    InitWindow(LARGURA+200, ALTURA+200, "Pacman");
+    srand(time(NULL));
+    SetTargetFPS(60);
+    // intervalo entre um desenho e outro
+    const float intervalo = 1.0f / 10.0f;
+    float contador_tempo = 0.0f;
+    
+    Vector2 pacman_pos_visual = { pacman.posicao.coluna * CELULA, pacman.posicao.linha * CELULA };
+    const float VELOCIDADE_PACMAN = 100.0f;
+    
+
+    // Variáveis do Power-Up
+    bool power_up_ativo = false;
+    int power_up_timer = 0;
+    const int TEMPO_POWER_UP = 80; // considerei 8 segundos (ver esquema de velocidade para poder aumentar fps)
+
+
+
+    // ~~~~ 5) Loop principal ~~~~ //
+    while (!WindowShouldClose()) {
+
+        contador_tempo = contador_tempo + GetFrameTime();
+        float deslize = contador_tempo / intervalo;
+        if(deslize > 1.0f){
+            deslize = 1.0f;
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        int x = pacman.posicao.linha;
+        int y = pacman.posicao.coluna;
+        
+        
+        // --- Desenho do Mapa ---
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 40; j++) {
+                int px = j * CELULA;
+                int py = i * CELULA;
+                switch (mapa[i][j]) {
+                    case '#': DrawRectangle(px + 100, py + 100, CELULA, CELULA, BLUE); break;
+                    case '.': DrawCircle(px + CELULA/2 + 100, py + CELULA/2 + 100, 3, YELLOW); break;
+                    case 'o': DrawCircle(px + CELULA/2 + 100, py + CELULA/2 + 100, 6, GREEN); break;
+                    case 'T': DrawCircle(px + CELULA/2 + 100, py + CELULA/2 + 100, 5, ORANGE); break;
+                }
+            }
+        }
+
+
+        Color cor_fantasma = power_up_ativo ? BLUE : PURPLE; //cor vai depender do estado do fantasma
+        
+        for (int i = 0; i < qnt_f; i++) {
+            
+            float aux_fantasma_x, aux_fantasma_y;
+            
+            if (array_fantasmas[i].teleportado) {
+                aux_fantasma_x = array_fantasmas[i].posicao.coluna * CELULA;
+                aux_fantasma_y = array_fantasmas[i].posicao.linha * CELULA;
+            } else {
+                aux_fantasma_x = (array_fantasmas[i].posicao_anterior.coluna * (1.0f - deslize) + array_fantasmas[i].posicao.coluna * deslize) * CELULA;
+                aux_fantasma_y = (array_fantasmas[i].posicao_anterior.linha * (1.0f - deslize) + array_fantasmas[i].posicao.linha * deslize) * CELULA;
+            }
+
+            DrawRectangle(aux_fantasma_x + 100, aux_fantasma_y + 100, CELULA, CELULA, cor_fantasma);
+        }
+        
+
+        // --- Desenha Pacman ---
+        float aux_pacman_x, aux_pacman_y;
+
+        if (pacman.teleportado) {
+            aux_pacman_x = pacman.posicao.coluna * CELULA;
+            aux_pacman_y = pacman.posicao.linha * CELULA;
+        } else {
+            aux_pacman_x = (pacman.posicao_anterior.coluna * (1.0f - deslize) + pacman.posicao.coluna * deslize) * CELULA;
+            aux_pacman_y = (pacman.posicao_anterior.linha * (1.0f - deslize) + pacman.posicao.linha * deslize) * CELULA;
+        }
+        DrawCircle(aux_pacman_x + CELULA / 2 + 100, aux_pacman_y + CELULA / 2 + 100, 9, GOLD);
+        EndDrawing();
+        
+
+        if(contador_tempo >= intervalo){
+            contador_tempo -= intervalo;
+            if (mapa[pacman.posicao.linha][pacman.posicao.coluna] != 'T') {
+                 pacman.teleportado = false;
+            }
+            
+            for (int i = 0; i < qnt_f; i++) {
+                if (mapa[array_fantasmas[i].posicao.linha][array_fantasmas[i].posicao.coluna] != 'T') {
+                    array_fantasmas[i].teleportado = false;
+                }
+            }
+
+            pacman.posicao_anterior = pacman.posicao;
+            for(int i = 0; i< qnt_f; i++){
+                array_fantasmas[i].posicao_anterior = array_fantasmas[i].posicao;
+            }
+            if (power_up_ativo) {
+                power_up_timer--;
+                if (power_up_timer <= 0) {
+                    power_up_ativo = false;
+                }
+            }
+            pacman.andar = false;
+            if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {pacman.direcao_atual = CIMA; pacman.andar = true;} 
+            if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {pacman.direcao_atual = BAIXO; pacman.andar = true;} 
+            if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {pacman.direcao_atual = DIREITA; pacman.andar = true;} 
+            if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {pacman.direcao_atual = ESQUERDA; pacman.andar = true;}
+            
+            if (pacman.andar == true){
+                pacman.teleportado = false;
+                if (!colidiu_com_parede(pacman, pacman.direcao_atual, mapa))
+                mover_para(&pacman, pacman.direcao_atual);
+            }
+            int x = pacman.posicao.linha;
+            int y = pacman.posicao.coluna;
+
+            if (mapa[x][y] == '.') {
+                mapa[x][y] = ' '; 
+            } else if (mapa[x][y] == 'o') {
+                mapa[x][y] = ' ';
+                power_up_ativo = true;
+                power_up_timer = TEMPO_POWER_UP;
+            }
+
+            checar_teleporte(&pacman, portais, qnt_portais);
+            
+            for (int i = 0; i < qnt_f; i++) {
+                mover_fantasma(&array_fantasmas[i], array_fantasmas, qnt_f, mapa);
+                checar_teleporte(&array_fantasmas[i], portais, qnt_portais);
+            }
+            verificar_colisao_pacman_fantasma(&pacman, array_fantasmas, qnt_f, power_up_ativo, pos_inicial_pacman );
+        
+        }
+        }
+    
+    
+
+    // ~~~~ 6) Libera memória ~~~~ //
+    CloseWindow();
+    free(array_fantasmas);
+    free(portais);
+    return 0;
+
+}
