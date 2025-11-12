8ae02c6aa054f1a30b3a5dc09efb14535f15b779
dottolucas052-afk <dottolucas052@gmail.com>
2025-11-12 18:40:21 -0300

    Implement pellet tracking and victory condition
    Added functionality to track pellets and win condition.

--- main/main.c
+++ main/main.c
@@ -28,9 +28,9 @@ int main() {
     // ~~~~ 2) Inicializações ~~~~ //
     tipo_objeto *array_fantasmas = NULL;
     int qnt_f = 0;
-
+    int pellets = 0;
     tipo_objeto pacman;
-
+    pacman.velocidade = 7.0f;
     tipo_posicao pos_inicial_pacman;
 
     // Conta portais (T)
@@ -63,6 +63,7 @@ int main() {
                 array_fantasmas[qnt_f-1].proxima_direcao = CIMA;
                 array_fantasmas[qnt_f-1].andar = true;
                 array_fantasmas[qnt_f-1].teleportado = false;
+                array_fantasmas[qnt_f -1].velocidade = 7.0f;
                 mapa[i][j] = ' ';
             }
             if (mapa[i][j] == 'T') {
@@ -80,6 +81,9 @@ int main() {
                 pacman.andar = false;
                 pacman.teleportado = false;                           
             }
+            if(mapa[i][j] == '.'|| mapa[i][j] == 'o'){
+                pellets++;
+            }
         }
     }
 
@@ -91,13 +95,18 @@ int main() {
     InitWindow(LARGURA+200, ALTURA+200, "Pacman");
     srand(time(NULL));
     SetTargetFPS(60);
-
+    double TempoInicio = 0.0;
+    double TempoAtual;
     // intervalo entre um desenho e outro
 
     bool jogo_pausado = true;
-
-    const float intervalo = 1.0f / 10.0f;
+    bool venceu = false;
+    const float intervalo = 1.0f / 4.0f;
+    float intervalo_pacman = 1.0f/pacman.velocidade;
+    float intervalo_fantasmas = 1.0f/7.0f;
     float contador_tempo = 0.0f;
+    float contador_pacman = 0.0f;
+    float contador_fantasmas = 0.0f;
     
     Vector2 pacman_pos_visual = { pacman.posicao.coluna * CELULA, pacman.posicao.linha * CELULA };
     const float VELOCIDADE_PACMAN = 100.0f;
@@ -117,16 +126,35 @@ int main() {
         if (IsKeyPressed(KEY_TAB)) {
             jogo_pausado = !jogo_pausado;
         }
-        contador_tempo = contador_tempo + GetFrameTime();
-        float deslize = contador_tempo / intervalo;
-        if(deslize > 1.0f){
-            deslize = 1.0f;
+        if(power_up_ativo){
+            intervalo_fantasmas = 1.0f/5.0f;
+        }
+        contador_pacman += GetFrameTime();
+        contador_fantasmas += GetFrameTime();
+        
+        float deslize_pacman = contador_pacman / intervalo_pacman;
+        float deslize_fantasma  = contador_fantasmas /intervalo_fantasmas;
+        
+        if(deslize_pacman > 1.0f){
+            deslize_pacman = 1.0f;
+        }
+        if(deslize_fantasma > 1.0f){
+            deslize_fantasma = 1.0f;
         }
         snprintf(texto_pontuacao, sizeof(texto_pontuacao), "Pontuação: %d", pontos);
         
         BeginDrawing();
         ClearBackground(AZUL_ESCURO);
-
+        if(venceu){
+            ClearBackground(WHITE);
+            DrawText("Você Venceu!", 250, 200, 40, RED);
+            if(TempoAtual - TempoInicio > 8.0){
+                EndDrawing();
+                break;
+            }
+            EndDrawing();
+            continue;
+        }
         DrawText(texto_pontuacao, 10, 10, 20, WHITE);
 
         int x = pacman.posicao.linha;
@@ -149,7 +177,7 @@ int main() {
 
 
         Color cor_fantasma = power_up_ativo ? RED : PURPLE; //cor vai depender do estado do fantasma
-        
+
         for (int i = 0; i < qnt_f; i++) {
             
             float aux_fantasma_x, aux_fantasma_y;
@@ -158,8 +186,8 @@ int main() {
                 aux_fantasma_x = array_fantasmas[i].posicao.coluna * CELULA;
                 aux_fantasma_y = array_fantasmas[i].posicao.linha * CELULA;
             } else {
-                aux_fantasma_x = (array_fantasmas[i].posicao_anterior.coluna * (1.0f - deslize) + array_fantasmas[i].posicao.coluna * deslize) * CELULA;
-                aux_fantasma_y = (array_fantasmas[i].posicao_anterior.linha * (1.0f - deslize) + array_fantasmas[i].posicao.linha * deslize) * CELULA;
+                aux_fantasma_x = (array_fantasmas[i].posicao_anterior.coluna * (1.0f - deslize_fantasma) + array_fantasmas[i].posicao.coluna * deslize_fantasma) * CELULA;
+                aux_fantasma_y = (array_fantasmas[i].posicao_anterior.linha * (1.0f - deslize_fantasma) + array_fantasmas[i].posicao.linha * deslize_fantasma) * CELULA;
             }
 
             DrawRectangle(aux_fantasma_x + 100, aux_fantasma_y + 100, CELULA, CELULA, cor_fantasma);
@@ -174,8 +202,8 @@ int main() {
             aux_pacman_x = pacman.posicao.coluna * CELULA;
             aux_pacman_y = pacman.posicao.linha * CELULA;
         } else {
-            aux_pacman_x = (pacman.posicao_anterior.coluna * (1.0f - deslize) + pacman.posicao.coluna * deslize) * CELULA;
-            aux_pacman_y = (pacman.posicao_anterior.linha * (1.0f - deslize) + pacman.posicao.linha * deslize) * CELULA;
+            aux_pacman_x = (pacman.posicao_anterior.coluna * (1.0f - deslize_pacman) + pacman.posicao.coluna * deslize_pacman) * CELULA;
+            aux_pacman_y = (pacman.posicao_anterior.linha * (1.0f - deslize_pacman) + pacman.posicao.linha * deslize_pacman) * CELULA;
         }
         DrawCircle(aux_pacman_x + CELULA / 2 + 100, aux_pacman_y + CELULA / 2 + 100, 9, GOLD);
        
@@ -190,30 +218,18 @@ int main() {
 
        
 
-        if(contador_tempo >= intervalo){
-            contador_tempo -= intervalo;
+        if(contador_pacman >= intervalo_pacman){
+            contador_pacman -= intervalo_pacman;
             if (mapa[pacman.posicao.linha][pacman.posicao.coluna] != 'T') {
                  pacman.teleportado = false;
             }
             
-            for (int i = 0; i < qnt_f; i++) {
-                if (mapa[array_fantasmas[i].posicao.linha][array_fantasmas[i].posicao.coluna] != 'T') {
-                    array_fantasmas[i].teleportado = false;
-                }
-            }
 
             pacman.posicao_anterior = pacman.posicao;
-            for(int i = 0; i< qnt_f; i++){
-                array_fantasmas[i].posicao_anterior = array_fantasmas[i].posicao;
-            }
+
 
             if (jogo_pausado) {
-                if (power_up_ativo) {
-                    power_up_timer--;
-                    if (power_up_timer <= 0) {
-                        power_up_ativo = false;
-                    }
-                }
+                
                 pacman.andar = false;
                 if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {pacman.direcao_atual = CIMA; pacman.andar = true;} 
                 if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {pacman.direcao_atual = BAIXO; pacman.andar = true;} 
@@ -229,24 +245,54 @@ int main() {
                 int y = pacman.posicao.coluna;
 
                 if (mapa[x][y] == '.') {
+                    pellets--;
                     mapa[x][y] = ' '; 
                     pontos += 10;
+                    if(pellets == 0 && !venceu){
+                        venceu = true;
+                        TempoInicio = GetTime();
+                    }
                 } else if (mapa[x][y] == 'o') {
                     mapa[x][y] = ' ';
+                    pellets--;
                     power_up_ativo = true;
                     power_up_timer = TEMPO_POWER_UP;
                     pontos += 50;
+                    if(pellets == 0 && !venceu){
+                        venceu = true;
+                        TempoInicio = GetTime();
+                    }
                 }
 
                 checar_teleporte(&pacman, portais, qnt_portais);
-                
+                verificar_colisao_pacman_fantasma(&pacman, array_fantasmas, qnt_f, power_up_ativo, pos_inicial_pacman, &pontos );
+            
+            }
+            }
+            if(contador_fantasmas >= intervalo_fantasmas){
+                contador_fantasmas -= intervalo_fantasmas;
+                for (int i = 0; i < qnt_f; i++) {
+                    if (mapa[array_fantasmas[i].posicao.linha][array_fantasmas[i].posicao.coluna] != 'T') {
+                        array_fantasmas[i].teleportado = false;
+                    }
+                }
+                for(int i = 0; i< qnt_f; i++){
+                    array_fantasmas[i].posicao_anterior = array_fantasmas[i].posicao;
+                }
+                if(jogo_pausado){
+                    if (power_up_ativo) {
+                        power_up_timer--;
+                        if (power_up_timer <= 0) {
+                            power_up_ativo = false;
+                        }
+                    }
+                }
                 for (int i = 0; i < qnt_f; i++) {
                     mover_fantasma(&array_fantasmas[i], array_fantasmas, qnt_f, mapa);
                     checar_teleporte(&array_fantasmas[i], portais, qnt_portais);
                 }
-                verificar_colisao_pacman_fantasma(&pacman, array_fantasmas, qnt_f, power_up_ativo, pos_inicial_pacman, &pontos );
-            
-            }
+                //verificar_colisao_pacman_fantasma(&pacman, array_fantasmas, qnt_f, power_up_ativo, pos_inicial_pacman, &pontos );
+
             }
         }
     
@@ -259,3 +305,5 @@ int main() {
     return 0;
 
 }
+
+
